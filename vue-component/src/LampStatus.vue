7d928834e97dc5bd32e4008a6b461ae5689f2aa3
hankpinkman <ardagracenz@gmail.com>
2022-08-21 12:17:07 +0300

    v
    v

---
+++ vue-component/src/LampStatus.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <p>
+    Lamp Status : {{ status }}
+    </p>
+    <button @click="openLamp">Open Lamp</button>
+</div>
+</template>
+<script>
+export default {
+    data : function(){
+        return {
+        status : "Off" 
+        }
+    },
+    methods : {
+        openLamp(){
+            this.status = "Open..."
+        }
+
+    }
+}
+</script>
---
+++ vue-component/src/home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <lamp-status></lamp-status>
+</div>
+</template>
+
+<script>
+
+import LambStatus from './LampStatus.vue'
+
+export default{
+components : {
+    "lamp-status" : LambStatus
+}
+}
+</script>
